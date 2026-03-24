00ecfffcb20a9b0c798a2a11f33065a799f1610d
vtmhieu <vtmhieu111@gmail.com>
2026-03-24 01:44:07 +0100

    move credentials

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,13 +3,13 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB2ZVDOiDhKXKOQa8ddrptrTWZJPUWRDbA",
-  authDomain: "awsquiz-18b6d.firebaseapp.com",
-  projectId: "awsquiz-18b6d",
-  storageBucket: "awsquiz-18b6d.firebasestorage.app",
-  messagingSenderId: "64994463486",
-  appId: "1:64994463486:web:1bb3d8e1e2a02a6fb0dfaf",
-  measurementId: "G-NC20RJ5B6V"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
